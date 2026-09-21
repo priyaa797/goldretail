b2dd84fc548cf2b5ba377b8a6331e04ea3873c61
priyaa797 <priyapatilsony@gmail.com>
2026-09-21 04:52:21 +0000

    build: add compiled frontend production assets

--- frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import settingsReducer from './slices/settingsSlice';
-
-export const store = configureStore({
-  reducer: {
-    settings: settingsReducer,
-  },
-});
